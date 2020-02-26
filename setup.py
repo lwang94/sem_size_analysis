50c7f417a207b0c2f6c1f4e2c773437328e3388d
lawrence <fayuan.wang@mail.utoronto.ca>
2020-02-25 18:29:48 -0800

    added Procfile and changed structure to have front end in root

--- setup.py
+++ setup.py
@@ -5,18 +5,12 @@ from .src import config as cf
 
 def list_reqs(fname='requirements.txt'):
     with open(fname) as fd:
-       return reqs = fd.read().splitlines()
-    # deps = [i for i in reqs if i.startswith('--find-links')]
-    # reqs = [i for i in reqs if i not in deps]
-    # return reqs, deps
-
-
+        return fd.read().splitlines()
 
 setup(
     name='saemi',
     version=cf.VERSION,
     packages=['src', ],
-    # dependency_links=list_reqs()[1],
     install_requires=list_reqs(),
     long_description=open('README.md').read()
 )
